79a36105a996bea7e12ccde4307e1d4a9339493c
harell <harel@loostiger.com>
2020-04-04 18:13:32 +1300

    install travis

---
+++ tic.R
@@ -0,0 +1,25 @@
+library(tic, warn.conflicts = FALSE)
+source("./AppData/tic/helpers.R")
+
+# Stage: Before Script ----------------------------------------------------
+get_stage("before_script") %>%
+    add_code_step(remotes::install_deps(dependencies = TRUE))
+
+# Stage: Script -----------------------------------------------------------
+get_stage("script") 
+
+# Stage: After Success ----------------------------------------------------
+get_stage("after_success")
+
+# Stage: After Failure ----------------------------------------------------
+get_stage("after_failure") %>%
+    add_code_step(print(sessioninfo::session_info(include_base = FALSE)))
+
+# Stage: Before Deploy ----------------------------------------------------
+get_stage("before_deploy")
+
+# Stage: Deploy -----------------------------------------------------------
+get_stage("deploy")
+
+# Stage: After Deploy -----------------------------------------------------
+get_stage("after_deploy")
