f26ff2ddbe76d22544eac8976584f146e19f6872
harell <harel@loostiger.com>
2021-01-07 21:08:35 +1300

    add pipe

---
+++ R/utils-pipe.R
@@ -0,0 +1,10 @@
+#' @title Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom purrr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
---
+++ utils-pipe.R
@@ -0,0 +1,9 @@
+#' @title Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom purrr %>%
+#' @usage lhs \%>\% rhs
+NULL
